ed99bd011ff91981e04c048f855c48d12ebc1502
Santiago López Rey <lopreysan@iespiringalla.ad>
2022-06-13 19:09:18 +0200

    Creada escena principal e primeiros assets e prefabs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Contador de rondas. Control de Game Over. Control de interfaz. Control de cambio de escena.
+    // Spawn de enemigos. 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
