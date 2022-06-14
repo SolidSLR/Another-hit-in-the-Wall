515b0d3b3f3301d44550a6b3eb52b56f37769d00
Santiago López Rey <devsantilopez@gmail.com>
2022-06-14 13:24:23 +0200

    Funcionamento base de EnemySpell

--- Assets/Scripts/Enemy Scripts/EnemyShot.cs
+++ Assets/Scripts/Enemy Scripts/EnemyShot.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyShot : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(Vector2.left * speed * Time.deltaTime, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up/3 * speed * Time.deltaTime, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag == "Tower" || other.gameObject.tag == "Wall"){
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Enemy Scripts/EnemyWizard.cs
+++ Assets/Scripts/Enemy Scripts/EnemyWizard.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyWizard : EnemyBase
 {
     public int attackPower;
+    public GameObject spellPrefab;
+    private bool canShoot = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +16,23 @@ public class EnemyWizard : EnemyBase
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * speed * Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.tag == "RangeAttackPos" && gameObject.tag == "EnemyWizard"){
+        if(other.gameObject.tag == "RangeAttackPos"){
+            canShoot = true;
             speed = 0;
+            StartCoroutine("EnemyStartShooting");
             Debug.Log("I'm a wizard and I've reached the attack point");
             // Empezar ataque a distancia.
+        }
+    }
+
+    private IEnumerator EnemyStartShooting(){
+        while (canShoot){
+            yield return new WaitForSeconds(1.5f);
+            Instantiate(spellPrefab, transform.position, Quaternion.identity);
         }
     }
 }
